1b142a480514e5638f2a0d26783488d6d15f5844
Valentina Zantedeschi <vzantedeschi@gmail.com>
2023-12-14 16:54:03 +0000

    cleanup and truncate long contexts

--- baselines/fid/src/datasets_loader.py
+++ baselines/fid/src/datasets_loader.py
@@ -9,68 +9,15 @@ import torch
 from random import shuffle
 
 
-class Dataset(torch.utils.data.Dataset):
-    def __init__(
-        self,
-        data,
-        n_context=None,
-        question_prefix="question:",
-        title_prefix="title:",
-        passage_prefix="context:",
-    ):
-        self.data = data
-        self.n_context = n_context
-        self.question_prefix = question_prefix
-        self.title_prefix = title_prefix
-        self.passage_prefix = passage_prefix
-        self.sort_data()
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, index):
-        example = self.data[index]
-        question = self.question_prefix + " " + example["question"]
-        target = self.get_target(example)
-
-        if "ctxs" in example and self.n_context is not None:
-            f = self.title_prefix + " {} " + self.passage_prefix + " {}"
-            contexts = example["ctxs"][: self.n_context]
-            passages = [f.format(c["title"], c["text"]) for c in contexts]
-            scores = [float(c["score"]) for c in contexts]
-            scores = torch.tensor(scores)
-
-            if len(contexts) == 0:
-                contexts = [question]
-        else:
-            passages, scores = None, None
-
-        return {
-            "index": index,
-            "question": question,
-            "answer": target,
-            "passages": passages,
-            "scores": scores,
-        }
-
-    def sort_data(self):
-        if self.n_context is None or "score" not in self.data[0]["ctxs"][0]:
-            return
-        for ex in self.data:
-            ex["ctxs"].sort(key=lambda x: float(x["score"]), reverse=True)
-
-    def get_example(self, index):
-        return self.data[index]
-
-
-def encode_passages(batch_text_passages, tokenizer):
+def encode_passages(batch_text_passages, tokenizer, text_maxlength=None):
     passage_ids, passage_masks = [], []
-    for k, text_passages in enumerate(batch_text_passages):
+    for text_passages in batch_text_passages:
         p = tokenizer.batch_encode_plus(
             text_passages,
+            max_length=text_maxlength,
             padding="max_length",
             return_tensors="pt",
-            truncation=True,
+            truncation=text_maxlength is not None,
         )
         passage_ids.append(p["input_ids"][None])
         passage_masks.append(p["attention_mask"][None])
@@ -87,12 +34,9 @@ class Collator(object):
         self.answer_maxlength = answer_maxlength
 
     def _get_target(self, example):
-        if "answer" in example:
-            return example["answer"]
-            # return target + " </s>" no need to add eos, already added by tokenizer
-
-        else:
-            return None
+        ans = example.get("answer", None)
+        return ans
+        # return target + " </s>" no need to add eos, already added by tokenizer
 
     def _format_input(self, example):
         n_contexts = len(example["contexts_list"])
@@ -126,6 +70,8 @@ class Collator(object):
         target_ids = target_ids.masked_fill(~target_mask, -100)
 
         text_passages = [self._format_input(example) for example in batch_list]
-        passage_ids, passage_masks = encode_passages(text_passages, self.tokenizer)
+        passage_ids, passage_masks = encode_passages(
+            text_passages, self.tokenizer, self.text_maxlength
+        )
 
         return (index, target_ids, target_mask, passage_ids, passage_masks)
