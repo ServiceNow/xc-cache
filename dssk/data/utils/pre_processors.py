abb91d4cbbb0fd777ff26d389d6da4fef0a79853
ceer2k <perouz@users.noreply.github.com>
2024-01-05 10:58:03 -0500

    Generate HotpotQA test set based on a given reordering of the contexts  (#136)
    * Add a funciton to rearrange contexts based on position
    
    * use hf map() to format the data
    
    * Add test for ordering
    
    * Add data truncation preprocessing
    
    * Update tests
    
    * Add script to create hotpot positional dataset
    
    * Fix typo
    
    * Cleanup
    
    * Add checks
    
    * Load and filter dataset
    
    * Add more checks and fix bug
    
    * Add handling of edge cases and refactor context reordering code
    
    * Cleanup
    
    * Remove restriction on num of contexts
    
    * Fix typos and documentation
    
    * Add tests
    
    * Add pushing to hf_hub

--- dssk/data/utils/pre_processors.py
+++ dssk/data/utils/pre_processors.py
@@ -1,6 +1,115 @@
 import torch
-from typing import List, Dict, Optional
+import numpy as np
+from typing import List, Dict, Optional, Any
 from dssk.data.utils.encoder import Encoder
+
+
+def merge_arrays(A, B, A_pos_list):
+    """
+    Returns a new array of len |A| + |B| where
+    the elements of A are at positions in A_pos_list.
+    If there is a position that exceeds the number of elements in the
+    merged array (overflow), then these positions will be ignored.
+
+    Given k positions:
+    If k > |A| (more positions than items in A),
+      then place A in the first |A| positions of A_pos_list.
+    If k < |A| (less positions than items in A),
+      then position the first |A|-k items based on A_pos_list
+      and concatenate the last k items of A to B.
+
+    """
+
+    pos_len = len(A_pos_list)
+    A_len = len(A)
+    B_len = len(B)
+    list_len = A_len + B_len
+
+    # If there is position overflow (pos index > available array positions)
+    # then we will ignore it
+    if list_len <= max(A_pos_list):
+        # Remove the large positions from the list
+        A_pos_list = [x for x in A_pos_list if x < list_len]
+        # Update the length of position list
+        pos_len = len(A_pos_list)
+
+    # If more positions than in A,
+    # use only first |A_pos_list| items in A
+    if pos_len > A_len:
+        A_pos_list = A_pos_list[:A_len]
+    # If less positions than in A,
+    # concatenate extra items in A to B
+    elif pos_len < A_len:
+        B = np.concatenate((B, A[pos_len:]))
+        A = A[:pos_len]
+
+    # Create a new array with the combined size
+    merged_array = np.empty(list_len, dtype=A.dtype)
+
+    # Copy elements from array A to specified positions
+    merged_array[A_pos_list] = A
+
+    # Copy elements from array B to the remaining positions
+    remaining_indices = np.setdiff1d(np.arange(list_len), A_pos_list)
+    merged_array[remaining_indices] = B
+
+    return merged_array
+
+
+def rearrange_list_by_pos(
+    np_list: np.ndarray,
+    mask: List[List[int]],
+    pos_list: List[int],
+    ignore_pos_overflow: bool = True,
+) -> np.ndarray:
+    """
+    np_list: A numpy array (list of lists of text).
+    mask: a 0/1 sequence, must be same dimensions as np_list.
+    pos_list: a list of positions (int) with max(pos_list) < np_list.shape[1]
+    ignore_pos_overflow: if True, then any position in pos_list > num of items
+        in np_list will be ignored. Otherwise, an exception will be raised.
+        For example, if np_list = ["a', 'b', 'c'] and pos_list = [0, 10],
+        then pos 10 will be ignored and only pos 0 will have the true context.
+
+    The function will split the np_list based on the mask, whereby
+    item at position i of np_list is sent to "false_list" if there is a
+    mask[i] == 0, to "true_list" otherwise.
+    The function returns a new list whereby the "true" elements appear
+    in positions pos_list.
+    For example:
+        np_list = [['a', 'b', 'c', 'd', 'e']]
+        mask = [[0, 1, 0, 0, 1]]
+        pos_list = [2, 3]
+        returns [['a', 'c', 'b', 'e', 'd']]
+
+    """
+
+    # Make sure there are no duplicate indices
+    len(np.unique(pos_list)) == len(pos_list)
+
+    list_len = np_list.shape[1]
+
+    # If either all or none of the items in the list are true,
+    # then return as is
+    if (list_len == np.sum(mask, axis=1)).all() or (np.sum(mask, axis=1) == 0).all():
+        return np_list
+
+    # If there is position overflow and we don't want to ignore it, raise an exception
+    if list_len <= max(pos_list) and not ignore_pos_overflow:
+        raise IndexError(
+            f"A position index {pos_list} is out of bounds for a list of length {list_len}."
+        )
+
+    # Split the array into two lists based on the mask
+    false_list = np.ma.masked_array(np_list, mask=mask)
+    true_list = np.ma.masked_array(np_list, mask=~false_list.mask)
+
+    ordered_list = [
+        merge_arrays(true_list[i].compressed(), false_list[i].compressed(), pos_list)
+        for i in range(false_list.shape[0])
+    ]
+
+    return ordered_list
 
 
 class WikipediaPreProcessor:
@@ -81,3 +190,142 @@ class TrainDataPreProcessor:
             "answer": answer_str,
             "encoder_hidden_states": context_embedding,
         }
+
+
+class PosContextPreProcessor:
+    """
+    The class is used to preprocess a dataset so that
+    the true context(s) is always at a given positions(s).
+    Useful for performaing ablations.
+    It is expected to be used with the HF dataset.map() function.
+    The processing can be applied to batches of size > 1,
+    as long as samples in the same batch have same shape.
+
+    The dataset to be processed is expected to have the following
+    fields populated:
+      - contexts_list
+      - titles_list
+      - context
+      - useful_contexts
+
+    The resulting dataset will alter the above four fields
+    (by rearranging the list items based on position)
+
+    """
+
+    def __init__(self, position_list: List[int], ignore_pos_overflow: bool = True) -> None:
+        """
+        position_list is a list of new positions where we want the true
+        contexts to be.  If None, then leave positions as is.
+        """
+        # If we have duplicate indices, raise an exception
+        if len(np.unique(position_list)) != len(position_list):
+            raise IndexError(
+                f"Duplicate indices!! The position list {position_list} contains duplicates"
+            )
+
+        self.position_list = position_list if position_list is None else sorted(position_list)
+        self.ignore_pos_overflow = ignore_pos_overflow
+
+    def __call__(self, examples: Dict[str, List]) -> List:
+        """
+        Rearrange the contexts based on the position of the true contexts
+        given in self.position_list.
+        Given a list of contexts and a list of true positions for each,
+        rearrange each context list so that the true contexts are at the
+        desired positons (self.position_list).
+        """
+
+        # If no position list given, return as is
+        if self.position_list is None:
+            return
+
+        try:
+            # Reorder the list of contexts/titles/masks so that
+            # it reflects the new positions
+            ordered_titles = rearrange_list_by_pos(
+                np.array(examples["titles_list"]),
+                examples["useful_contexts"],
+                self.position_list,
+                self.ignore_pos_overflow,
+            )
+            ordered_contexts = rearrange_list_by_pos(
+                np.array(examples["contexts_list"]),
+                examples["useful_contexts"],
+                self.position_list,
+                self.ignore_pos_overflow,
+            )
+            ordered_useful_contexts = rearrange_list_by_pos(
+                np.array(examples["useful_contexts"]),
+                examples["useful_contexts"],
+                self.position_list,
+                self.ignore_pos_overflow,
+            )
+        except IndexError as e:
+            print(f"There was a problem with sample {examples['sample_idx']}: {e}")
+
+        # Concatenate the contexts again based on the new ordering
+        concat_ordered_contexts = [" ".join(x) for x in ordered_contexts]
+
+        return {
+            "titles_list": ordered_titles,
+            "contexts_list": ordered_contexts,
+            "context": concat_ordered_contexts,
+            "useful_contexts": ordered_useful_contexts,
+        }
+
+    @staticmethod
+    def truncate_true_list(
+        item_list: List[Any], mask: List[int], max_items: int = None
+    ) -> np.ndarray:
+        """
+        - item_list: A list or np array of any type (e.g. list of contexts),
+        - mask: a 0/1 sequence, must be same dimensions as item_list.
+        - max_items: The max number of list items we want to return.
+
+        Returns a (shuffled) item_list containing :max_items number of items,
+        so that the unmasked items (e.g. "true" contexts) are in the output.
+
+        The function will split the item_list based on the mask, whereby
+        item at position i of item_list is sent to "false_list"
+        if mask[i] == 0, to "true_list" otherwise.
+        The function then truncates the false list based on max_items,
+        concatenates with true_list, and returns a shuffled list.
+
+        For example:
+            item_list = ['a', 'b', 'c', 'd', 'e']
+            mask = [0, 1, 0, 0, 1]
+            max_items = 3
+            returns ['e', 'c', 'b']
+        """
+
+        # Split the array into two lists based on the mask
+        false_list = np.ma.masked_array(item_list, mask=mask).compressed()
+        true_list = np.ma.masked_array(item_list, mask=np.logical_not(mask)).compressed()
+
+        true_len = len(true_list)
+        item_len = len(item_list)
+
+        # If no max_items speicified, no turncation will occur
+        if max_items is None:
+            max_items = item_len
+
+        # You need to return at least one item.
+        assert max_items > 0 and max_items <= item_len
+
+        if max_items > true_len:
+            # We have to truncate the false items
+            final_list = np.concatenate(
+                [
+                    true_list,
+                    np.random.choice(a=false_list, size=max_items - true_len, replace=False),
+                ]
+            )
+            # Shuffle the final list
+            np.random.shuffle(final_list)
+        else:
+            # No false items are reported. We sample from true items instead.
+            # This will also shuffle the true items
+            final_list = np.random.choice(true_list, size=max_items, replace=False)
+
+        return final_list
