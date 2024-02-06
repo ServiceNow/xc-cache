d183d0d4d71dafdf8c66368048500cbc641de9d2
Valentina Zantedeschi <vzantedeschi@gmail.com>
2024-02-06 20:18:51 +0000

    support for X-crossattn. Measure generation flops, do not include tokenization time, use permanent cache as costs do no change

--- dssk/metrics/generation/system_metrics.py
+++ dssk/metrics/generation/system_metrics.py
@@ -1,13 +1,9 @@
 from calflops import calculate_flops
 
 
-def estimate_flops_macs(model, tokenizer, batch_size, context_length, qa_length, **kwargs):
-    # supports only decoder-only models
-    total_flops, total_macs, _ = calculate_flops(
-        model=model,
-        input_shape=(batch_size, context_length + qa_length),
-        transformer_tokenizer=tokenizer,
-        output_as_string=False,
+def estimate_flops_macs_params(model, gen_kwargs, forward_mode="generate"):
+    total_flops, total_macs, total_params = calculate_flops(
+        model=model, output_as_string=False, forward_mode=forward_mode, kwargs=gen_kwargs
     )
 
-    return total_flops, total_macs
+    return total_flops, total_macs, total_params
