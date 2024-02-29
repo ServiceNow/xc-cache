fc66d2c89e40381d24d0f9a9a2dc47222a05b1bd
Joao Monteiro <joaomonteirof@gmail.com>
2024-02-29 14:36:10 +0000

    Using existing custom trainer and enabling qa evals for cross-attn models.

--- dssk/train/peft/trainer.py
+++ dssk/train/peft/trainer.py
@@ -1,9 +1,12 @@
 import os
 import torch
 import transformers
+from typing import Optional
 from transformers import TrainerCallback
 from peft import set_peft_model_state_dict
 from dssk.utils.scripting import LoggingCallback
+from dssk.train.cross_attn.trainer import CustomTrainer
+from dssk.data.cross_attn.datasets_loader import Collator as CrossCollator
 
 
 class LoadBestPeftModelCallback(TrainerCallback):
@@ -24,52 +27,6 @@ class WriteFinalStateCallback(TrainerCallback):
         return control
 
 
-class CustomTrainer(transformers.Trainer):
-    """
-    Custom trainer class for training to force label shifting for causal LM when label smoothing is used.
-    Copied from research-dssk, but removing the missing grad in the encoder.
-    """
-
-    def compute_loss(self, model, inputs, return_outputs=False):
-        """
-        How the loss is computed by Trainer. By default, all models return the loss in the first element.
-
-        Subclass and override for custom behavior.
-        """
-
-        if self.label_smoother is not None and "labels" in inputs:
-            labels = inputs.pop("labels")
-        else:
-            labels = None
-
-        # Drop fields related to generationin case those are in the batch since they're not used here.
-        inputs.pop("raw_answer", None)
-        inputs.pop("no_answer_input_ids", None)
-        inputs.pop("no_answer_attention_mask", None)
-
-        outputs = model(**inputs)
-        # Save past state if it exists
-        # TODO: this needs to be fixed and made cleaner later.
-        if self.args.past_index >= 0:
-            self._past = outputs[self.args.past_index]
-
-        if labels is not None:
-            # This is what we change wrt to the original compute_loss function.
-            # We set shift_labels=True since our custom models doesn't exist
-            # in the set of causal LM models within HF.
-            loss = self.label_smoother(outputs, labels, shift_labels=True)
-        else:
-            if isinstance(outputs, dict) and "loss" not in outputs:
-                raise ValueError(
-                    "The model did not return a loss from the inputs, only the following keys: "
-                    f"{','.join(outputs.keys())}. For reference, the inputs it received are {','.join(inputs.keys())}."
-                )
-            # We don't use .loss here since the model may return tuples instead of ModelOutput.
-            loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
-
-        return (loss, outputs) if return_outputs else loss
-
-
 def get_trainer(
     model,
     tokenizer,
@@ -82,6 +39,8 @@ def get_trainer(
     wandb_run_name=None,
     wandb_log_grads=False,
     cross_trainer=False,
+    cross_trainer_do_extra_evals: bool = False,
+    cross_trainer_generation_eval_max_sample_size: Optional[int] = None,
 ):
     """Intanstiates Trainer object."""
 
@@ -109,13 +68,16 @@ def get_trainer(
             )
 
         logging_callbacks = [
-            LoggingCallback(log_grads=wandb_log_grads),
+            LoggingCallback(
+                log_grads=wandb_log_grads,
+                do_extra_evals=cross_trainer_do_extra_evals and cross_trainer,
+                max_sample_size=cross_trainer_generation_eval_max_sample_size,
+            ),
         ]
     else:
         logging_callbacks = []
 
     if cross_trainer:
-        from dssk.data.cross_attn.datasets_loader import Collator as CrossCollator
 
         trainer = CustomTrainer(
             model=model,
